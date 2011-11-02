8938a4123e6e4f555cae4d95040d29a0f9949a44
Simon Johnston <simon@johnstonshome.org>
2011-11-02 14:21:59 -0700

    Removed TODO, moved all actions to Google issue tracker.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 import guernsey
 
-setup(name='Guernsey REST Client',
+setup(name='GuernseyRestClient',
       version='0.1.1',
       description='Guernsey REST Client for Python, based on the Java Jersey API.',
       author='Simon Johnston',
